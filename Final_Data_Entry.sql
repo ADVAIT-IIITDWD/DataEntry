aafaaa72bcb9560cb660bc1695aa393c69f2b74f
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2022-05-06 00:33:54 +0530

    query statement added

--- Final_Data_Entry.sql
+++ Final_Data_Entry.sql
@@ -837,6 +837,8 @@ INSERT INTO ADVAIT_receipt VALUES("RID_16","PRP016","016",2700,"2020-04-14");
   
   /* QUERIES */
 /* 1 */
+/* retrieve cutomer and vehicle details who has been involved in an incident and claim status is pending */
+
 SELECT c.cust_id, v.vehicle_id FROM ADVAIT_customer C, ADVAIT_vehicle V WHERE V.CUST_ID = C.CUST_ID AND 
 	C.CUST_ID IN ( SELECT C1.CUST_ID FROM ADVAIT_claim C1 WHERE C1.claim_status
     = 'PENDING' AND C1.INCIDENT_ID IN (SELECT I.INCIDENT_ID FROM ADVAIT_incident_report I 
@@ -844,8 +846,10 @@ SELECT c.cust_id, v.vehicle_id FROM ADVAIT_customer C, ADVAIT_vehicle V WHERE V.
 
 
 /* 2 */ 
+/* customer details who has premium amount greater than the sum of all the customerids in database */
+
 /* M-1 */
-	select * from advait_customer
+	select advait_customer.* from advait_customer
 join advait_premium_payment 
 	on advait_customer.cust_id=advait_premium_payment .cust_id
 where 
@@ -860,20 +864,27 @@ select sum(cust_id) from advait_customer));
 
 
 /* 3 */
+/* Company details whose number of products is greater than departments where departments are located in more than one location */
+
 SELECT C.COMPANY_NAME FROM advait_INSURANCE_COMPANY C INNER JOIN advait_PRODUCT P ON P.COMPANY_NAME=C.COMPANY_NAME GROUP BY P.COMPANY_NAME
 HAVING COUNT(*) >ALL(SELECT COUNT(*) FROM advait_INSURANCE_COMPANY GROUP BY COMPANY_NAME HAVING COUNT(COMPANY_LOCATION)>1);
 
+
 /* 4 */
+/* customers who have more than one Vehicle, premium for one of Vehicle is not paid and is involved in accident */
+
 SELECT CONCAT(C.CUST_ID,",",CUST_FNAME,CUST_LNAME) AS CUST_INFO FROM (((advait_CUSTOMER C INNER JOIN advait_VEHICLE V ON (C.CUST_ID=V.CUST_ID) ) 
     INNER JOIN advait_PREMIUM_PAYMENT P ON (P.CUST_ID=V.CUST_ID AND P.PREMIUM_PAYMENT_AMOUNT=0)) INNER JOIN advait_INCIDENT_REPORT IR ON (IR.CUST_ID=P.CUST_ID AND IR.INCIDENT_TYPE='ACCIDENT'))
     GROUP BY V.CUST_ID HAVING COUNT(V.VEHICLE_ID)>1;
          
 /* 5 */ 
+/* Vehicles which have premium more than its Vehicle Number */
 
 SELECT distinct(V.VEHICLE_ID) FROM advait_VEHICLE V, advait_PREMIUM_PAYMENT P WHERE 
 	V.CUST_ID = P.CUST_ID AND P.PREMIUM_PAYMENT_ID > V.VEHICLE_NUMBER;
 
 /* 6 */ 
+/* Customer details whose Claim Amount is less than Coverage Amount and Claim Amount is greater than Sum(CLAIM_SETTLEMENT_ID, VEHICLE_ID, CLAIM_ID, CUST_ID) */
 
 SELECT CONCAT(C.CUST_ID,",",CUST_FNAME,",",CUST_LNAME) AS CUST_INFO FROM (advait_CUSTOMER C INNER JOIN advait_CLAIM_SETTLEMENT CS ON CS.CUST_ID=C.CUST_ID
 INNER JOIN advait_CLAIM CL ON (CL.CLAIM_ID=CS.CLAIM_ID)INNER JOIN advait_COVERAGE CO ON (CO.COVERAGE_ID=CS.COVERAGE_ID AND 
