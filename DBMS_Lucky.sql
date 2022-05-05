a98c5ad6f12af4d8eca84e26ca2d727e812dc1cf
Brij Vaghani-iiitdwd <88758778+Brij2003@users.noreply.github.com>
2022-05-05 21:46:34 +0530

    Update DBMS_Lucky.sql

--- DBMS_Lucky.sql
+++ DBMS_Lucky.sql
@@ -1060,26 +1060,34 @@ ADD CONSTRAINT `fk_company_name5`
   
   
   /* QUERIES */
-
 /* 1 */
+SELECT c.cust_id, v.vehicle_id FROM ADVAIT_customer C, ADVAIT_vehicle V WHERE V.CUST_ID = C.CUST_ID AND 
+	C.CUST_ID IN ( SELECT C1.CUST_ID FROM ADVAIT_claim C1 WHERE C1.claim_status
+    = 'PENDING' AND C1.INCIDENT_ID IN (SELECT I.INCIDENT_ID FROM ADVAIT_incident_report I 
+    WHERE I.CUST_ID = C1.CUST_ID AND I.vehicle_id = V.vehicle_id ));
 
-select c.cust_id, v.vehicle_id from ADVAIT_customer as c, advait_vehicle as v
-where c.cust_id in (select cust_id from advait_claim where calim_status is null);
 
 /* 2 */ 
 
-select c.cust_id from advait_customer as c 
-where c.cust_id in (
-			select cust_id from advait_premium_payment where premium_payment_amount > (
-						select sum(cust_id) from advait_customer));
-                        
+
+
+/* 3 */
+SELECT C.COMPANY_NAME FROM advait_INSURANCE_COMPANY C INNER JOIN advait_PRODUCT P ON P.COMPANY_NAME=C.COMPANY_NAME GROUP BY P.COMPANY_NAME
+HAVING COUNT(*) >ALL(SELECT COUNT(*) FROM advait_INSURANCE_COMPANY GROUP BY COMPANY_NAME HAVING COUNT(COMPANY_LOCATION)>1);
+
+/* 4 */
+SELECT CONCAT(C.CUST_ID,",",CUST_FNAME,CUST_LNAME) AS CUST_INFO FROM (((advait_CUSTOMER C INNER JOIN advait_VEHICLE V ON (C.CUST_ID=V.CUST_ID) ) 
+    INNER JOIN advait_PREMIUM_PAYMENT P ON (P.CUST_ID=V.CUST_ID AND P.PREMIUM_PAYMENT_AMOUNT=0)) INNER JOIN advait_INCIDENT_REPORT IR ON (IR.CUST_ID=P.CUST_ID AND IR.INCIDENT_TYPE='ACCIDENT'))
+    GROUP BY V.CUST_ID HAVING COUNT(V.VEHICLE_ID)>1;
+         
 /* 5 */ 
 
-select v.vehicle_type from advait_vehicle as v where v.cust_id in (
-						select cust_id from advait_premium_payment where premium_payment_amount > (
-													select vehicle_number from advait_vehicle));
+SELECT distinct(V.VEHICLE_ID) FROM advait_VEHICLE V, advait_PREMIUM_PAYMENT P WHERE 
+	V.CUST_ID = P.CUST_ID AND P.PREMIUM_PAYMENT_ID > V.VEHICLE_NUMBER;
 
 /* 6 */ 
 
-select * from advait_customer as c where c.cust_id in (select cust_id from advait_claim where claim_amount < (select coverage_amount from advait_coverage) and claim_amount > 
-(select claim_statement_id + vehicle_id + claim_id + cust_id from advait_claim_statement)); 
+SELECT CONCAT(C.CUST_ID,",",CUST_FNAME,",",CUST_LNAME) AS CUST_INFO FROM (advait_CUSTOMER C INNER JOIN advait_CLAIM_SETTLEMENT CS ON CS.CUST_ID=C.CUST_ID
+INNER JOIN advait_CLAIM CL ON (CL.CLAIM_ID=CS.CLAIM_ID)INNER JOIN advait_COVERAGE CO ON (CO.COVERAGE_ID=CS.COVERAGE_ID AND 
+CL.CLAIM_AMOUNT<CO.COVERAGE_AMOUNT AND CL.CLAIM_AMOUNT>(CS.CLAIM_SETTLEMENT_ID+CS.VEHICLE_ID+CS.CLAIM_ID+CS.CUST_ID)));
+    
